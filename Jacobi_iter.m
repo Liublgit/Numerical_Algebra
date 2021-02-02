454bffa12c4b58e075cc42e9b3539e7b2e792579
Liublgit <46802627+Liublgit@users.noreply.github.com>
2021-02-02 16:38:09 +0800

    Add files via upload

---
+++ Jacobi_iter.m
@@ -0,0 +1,31 @@
+function x = Jacobi_iter(A,b,tol,it_max)
+% Jacobi_iter solve the problem Ax = b
+% using Jacobi iteration method.
+% tol : norm(x_k-x_{k-1}) < tol
+% it_max : max of iteration.
+%=============== initiate  ===============%
+iter = 1;
+n = size(A);
+x0 = ones(n(1),1);
+D = diag(diag(A));
+L = -tril(A,-1);
+U = -triu(A,1);
+B = D\(L + U);
+g = D\b;
+rho = vrho(B);
+fprintf('\n Spectral radius of iteration matrix = %d \n',rho)
+%=============== iteration  ===============%
+while iter <= it_max
+    x = B*x0 + g;
+    fprintf('\n Iteration number = %d, ||x_k - x_{k-1}||= %d \n',iter, norm(x-x0))
+    if norm(x-x0) < tol
+        break;
+    end
+    x0 = x;
+    iter = iter + 1;
+end
+%=============== Exceed  ===============%
+if iter > it_max
+   fprintf('\n Maximum number of iterations exceeded! \n');
+end
+end
